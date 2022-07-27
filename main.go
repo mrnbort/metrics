7129985322fce4d69b26f2088ecbe86a214a0fc0
marynabort <maryna@bort.name>
2022-07-27 13:34:00 -0500

    ticker added, not working properly without timeout

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ var TestData = []struct {
 	timeStamp string
 	value     int
 }{
+	{"file_0", "2022-07-17T05:30:10", 10},
 	{"file_1", "2022-07-17T05:39:07", 2},
 	{"file_1", "2022-07-17T05:39:08", 1},
 	{"file_1", "2022-07-17T05:39:09", 3},
@@ -38,47 +39,65 @@ func main() {
 
 	data := make(MemoryData)
 
-	for _, m := range TestData {
-		t, err := time.Parse(layout, m.timeStamp)
-		if err != nil {
-			fmt.Println(err)
-		}
-		t_hour := t.Hour()*60 + t.Minute()
+	ticker := time.NewTicker(1 * time.Minute)
+	defer ticker.Stop()
 
-		if _, ok := data[m.metric]; ok {
-			if t_hour == data[m.metric].MinuteValue {
-				data[m.metric].Value = data[m.metric].Value + m.value
-			} else {
-				db = append(db, DBData{
-					Metric:    m.metric,
-					TimeStamp: data[m.metric].TimeStamp,
-					Value:     data[m.metric].Value,
-				})
-				data[m.metric] = &Metric{
-					Value:       m.value,
-					TimeStamp:   t,
-					MinuteValue: t_hour,
+	timeoutCh := time.After(10 * time.Minute)
+
+	for {
+		select {
+		//case tick := <-ticker.C:
+		case <-ticker.C:
+			//tick_hour := tick.Hour()*60 + tick.Minute()
+			tickHour := 340
+			if data != nil {
+				for k, v := range data {
+					if tickHour != data[k].MinuteValue {
+						db = append(db, DBData{
+							Metric:    k,
+							TimeStamp: v.TimeStamp,
+							Value:     v.Value,
+						})
+						delete(data, k)
+					}
 				}
 			}
-		} else {
-			data[m.metric] = &Metric{
-				Value:       m.value,
-				TimeStamp:   t,
-				MinuteValue: t_hour,
+		case <-timeoutCh:
+			fmt.Println("Finished routine!")
+			return
+		default:
+			for _, m := range TestData {
+				t, err := time.Parse(layout, m.timeStamp)
+				if err != nil {
+					fmt.Println(err)
+				}
+				tHour := t.Hour()*60 + t.Minute()
+
+				if _, ok := data[m.metric]; ok {
+					if tHour == data[m.metric].MinuteValue {
+						data[m.metric].Value = data[m.metric].Value + m.value
+					} else {
+						db = append(db, DBData{
+							Metric:    m.metric,
+							TimeStamp: data[m.metric].TimeStamp,
+							Value:     data[m.metric].Value,
+						})
+						data[m.metric] = &Metric{
+							Value:       m.value,
+							TimeStamp:   t,
+							MinuteValue: tHour,
+						}
+					}
+				} else {
+					data[m.metric] = &Metric{
+						Value:       m.value,
+						TimeStamp:   t,
+						MinuteValue: tHour,
+					}
+				}
 			}
+			fmt.Println("done1")
 		}
 	}
-
-	//	data = append(
-	//		data,
-	//		MemoryData{"metric": m.metric, "time_value": MemoryData{{"minute": t_hour, "value": m.value}}})
-	//	fmt.Println(data)
-	//}
-
-	//for i := 1; i < 1441; i++ {
-	//	fmt.Println(i)
-	//	if i == 1440 {
-	//		i = 1
-	//	}
-	//}
+	fmt.Println("done2")
 }
